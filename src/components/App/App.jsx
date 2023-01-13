cb62ba6cdc896a7e29dae35e16dd37e7a3893a95
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-01-13 15:17:18 +0200

    перенес логику в Арр

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,51 @@
 import React, { Component }  from "react";
-// import css from "./App.module.css";
+import css from "./App.module.css";
 // import  Container  from "../Container";
 import Searchbar from "../Searchbar";
 import ImageGallery from "components/ImageGallery";
-import { cssTransition } from "react-toastify";
-// import Button from "components/Button";
+import fetchPictures from '../functions';
+import Button from "components/Button";
+
 // import ImageGalleryItem from "components/ImageGalleryItem";
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
+// import { css } from "react-toastify";
+
 
 
 export class App extends Component {
   state = {
     searchName: '',
+    imageList: [],
+    error: null,
+    loading: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+
+    const {searchName} = this.state.searchName;
+
+    if (prevState.searchName !== searchName) {
+      console.log('изменилось значение для поиска');
+
+      try{
+        this.setState( {loading: true} );
+
+        fetchPictures(searchName);
+    
+        if (response.ok) {
+          const data = response.json();
+          console.log('data :', data);
+    
+          this.setState( {imageList: data.hits} );
+          console.log("imageList : ", this.setState.imageList)
+        } 
+        else {
+          return Promise.reject(new Error(`Нет картинок по запросу ${searchName}`))
+        }
+      }
+      catch(error) { this.setState( {error} ) }
+      finally { this.setState( {loading: false} ) };
+    };
   };
 
   handleSearchSubmit = searchName => {
@@ -21,16 +55,27 @@ export class App extends Component {
   loadMore () {}
 
   render () {
-    const { searchName } = this.state;
+    const { searchName,  imageList, loading, error } = this.state;
 
     return (  
-      <div className={cssTransition.App}>
+      <div className={css.App}>
         {/* <Container> */}
         <Searchbar onSubmit={this.handleSearchSubmit}/>
+        { error && (<h1> Поисковой запрос не дал результатов. Поробуйте другое значение</h1>)}
 
-        <ImageGallery searchName={searchName}/>
+        { loading && <p>загрузка...</p>}
 
-        <ToastContainer autoClose={3000}/>
+        { !imageList.length && <div><p>введите поисковой запрос</p></div>}
+
+        { imageList.length > 0 && 
+          <>
+            <ImageGallery imageList={imageList}/> 
+
+            <Button onClick={this.loadMore}/>
+          </>
+        }
+
+        {/* <ToastContainer autoClose={3000}/> */}
         {/* </Container> */}
         </div>
     )
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -2,76 +2,22 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import css from "./ImageGallery.module.css";
 import ImageGalleryItem from "components/ImageGalleryItem";
-import Button from "components/Button";
-import fetchPictures from '../functions';
 
 
 class ImageGallery extends Component {
-  state = {
-    searchName: '',
-    imageList: [],
-    error: null,
-    loading: false,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-
-    const {searchName} = this.props.searchName;
-
-    if (prevProps.searchName !== searchName) {
-      console.log('изменилось значение для поиска');
-
-      this.setState( {searchName} );
-      // this.props.setState( {searchName: ''} );
-
-      try{
-        this.setState( {loading: true} );
-
-        fetchPictures(searchName);
-    
-        if (response.ok) {
-          const data = response.json();
-          console.log('data :', data);
-    
-          this.setState( {imageList: data.hits} );
-          console.log("imageList : ", this.setState.imageList)
-        } 
-        else {
-          return Promise.reject(new Error(`Нет картинок по запросу ${searchName}`))
-        }
-      }
-      catch(error) { this.setState( {error} ) }
-      finally { this.setState( {loading: false} ) };
-    };
-  };
 
     render() {
-      const { imageList, loading, error } = this.state;
+      const { imageList } = this.props;
 
       return (
-        <>
-        { error && (<h1> Поисковой запрос не дал результатов. Поробуйте другое значение</h1>)}
-
-        { loading && <p>загрузка...</p>}
-
-        { !imageList.length && <div><p>введите поисковой запрос</p></div>}
-
-        { imageList.length > 0 && 
-          (<div>
-            <ul className={css.gallery}>
-                {imageList.map( imageListItem => (
-                  <ImageGalleryItem data={imageListItem}/>
-                ))}
-            </ul>
-
-            <Button onClick={this.loadMore}/>
-          </div>
-          )
-        }
-        </>
-      );
-    };
-  };
+        <ul className={css.gallery}>
+          {imageList.map( imageListItem => (
+            <ImageGalleryItem data={imageListItem}/>
+          ))}
+        </ul>
+      )
+    }
+};
 
 
 ImageGallery.propTypes = {
